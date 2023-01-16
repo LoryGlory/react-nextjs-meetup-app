36e00be1c1f32560bdb41ee13306a642df1f7e2e
Laura Roganovic <laura.roganovic@googlemail.com>
2023-01-16 14:50:39 +0100

    added getStaticPaths and getStaticProps function to tell NextJS which dynamic pages this page  should be pre-generated

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,40 @@ function MeetupDetails() {
   )
 };
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        }
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        }
+      }
+    ]
+  }
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  // fetch data
+
+  return {
+    props: {
+      meetupData: {
+        image: 'https://images.unsplash.com/photo-1503609796759-4cfd4d5a0b9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80',
+        id: 'm1',
+        title: 'First Meetup',
+        address: 'Some Street 1, San Francisco',
+        description: 'The meetup description'
+      }
+    }
+  }
+}
+
 export default MeetupDetails;
