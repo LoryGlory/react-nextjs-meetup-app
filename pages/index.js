ca05987a20752e854dd5ae4078cf3ff65ae6afe9
Laura Roganovic <laura.roganovic@googlemail.com>
2023-01-17 12:35:30 +0100

    added function to get data from MongoDB database

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import {MongoClient} from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -39,9 +40,25 @@ function HomePage(props) {
 // getStaticProps will return the data on the first render cycle; data-fetching for pre-rendering
 export async function getStaticProps() {
   //fetch data from an api for example
+  const client = await MongoClient.connect(
+      'mongodb+srv://laura:VS4u4uayit9gMe8L@cluster0.nf7log0.mongodb.net/?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map(meetup => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      }))
     },
     revalidate: 10
   };
